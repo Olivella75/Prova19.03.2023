45e2bfe49722c48f6dcb0ae7faf2d985b9be008a
Olivella75 <jordiolivella@hotmail.com>
2023-03-19 21:19:00 +0100

    Afegim css i js

---
+++ prova.js
@@ -0,0 +1,12 @@
+function main() {
+    let i, j;
+    let solucio = "";
+    let dibuix = "*";
+    for(i = 0; i < 10; i++) {
+        solucio += "</br>";
+        for (j = 0; j < i; j++) {
+            solucio += dibuix;
+        }
+    }
+    document.getElementById("resultat").innerHTML = solucio;
+}
